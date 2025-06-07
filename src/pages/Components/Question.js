1db55a007b90128c05408c57d653ae703a848224
Georgegithup <120557663+Georgegithup@users.noreply.github.com>
2025-06-07 17:02:30 +0300

    Header

---
+++ src/pages/Components/Question.js
@@ -0,0 +1,31 @@
+
+
+export default function Question({ question, onNext, onPrevious, isFirst, isLast }) {
+  return (
+    <div className="question">
+      <h4>Question {question.QuestionNum}</h4>
+      <h1>{question.Question}</h1>
+
+      <div className="options-container">
+        <p className="option">{question.A}</p>
+        <p className="option">{question.B}</p>
+      </div>
+      <div className="options-container">
+        <p className="option">{question.C}</p>
+        <p className="option">{question.D}</p>
+      </div>
+
+      <div className="button-container">
+        {!isFirst && (
+          <button onClick={onPrevious}>Previous</button>
+        )}
+
+        {!isLast ? (
+          <button onClick={onNext}>Next</button>
+        ) : (
+          <button onClick={() => alert("Quiz submitted!")}>Submit</button>
+        )}
+      </div>
+    </div>
+  );
+}
