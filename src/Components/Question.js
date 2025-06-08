caa7e9f4deb03f8b9da59a972bdd8d31748f7224
Georgegithup <120557663+Georgegithup@users.noreply.github.com>
2025-06-08 16:40:42 +0300

    quizzes

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,27 +1,41 @@
 
 
-export default function Question({ question, onNext, onPrevious, isFirst, isLast }) {
+export default function Question({
+  question,
+  onNext,
+  onPrevious,
+  isFirst,
+  isLast,
+  onAnswerSelect,
+  selectedAnswer
+}) {
+  const renderOption = (key) => (
+    <p
+      className={`option ${selectedAnswer === key ? 'selected' : ''}`}
+      onClick={() => onAnswerSelect(key)}
+    >
+      {question[key]}
+    </p>
+  );
+
   return (
     <div className="question">
       <h4>Question {question.QuestionNum}</h4>
       <h1>{question.Question}</h1>
 
       <div className="options-container">
-        <p className="option">{question.A}</p>
-        <p className="option">{question.B}</p>
+        {renderOption('A')}
+        {renderOption('B')}
       </div>
       <div className="options-container">
-        <p className="option">{question.C}</p>
-        <p className="option">{question.D}</p>
+        {renderOption('C')}
+        {renderOption('D')}
       </div>
 
       <div className="button-container">
-        {!isFirst && (
-          <button onClick={onPrevious}>Previous</button>
-        )}
-
+        {!isFirst && <button onClick={onPrevious}>&laquo;</button>}
         {!isLast ? (
-          <button onClick={onNext}>Next</button>
+          <button onClick={onNext}>&raquo;</button>
         ) : (
           <button onClick={() => alert("Quiz submitted!")}>Submit</button>
         )}
