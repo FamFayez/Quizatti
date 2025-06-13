b6cdf8680ad54f9473bc4711d85a1395d192156d
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2025-06-13 06:04:29 +0300

    Update baseUrl in constants.js to use production API endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://api.quizatty.com/api/v1";
-export const baseUrl = "http://localhost:8000/api/v1";
+export const baseUrl = "https://api.quizatty.com/api/v1";
+// export const baseUrl = "http://localhost:8000/api/v1";
 export const token = sessionStorage.getItem("token");
 export const userData = JSON.parse(sessionStorage.getItem("user"));
 
