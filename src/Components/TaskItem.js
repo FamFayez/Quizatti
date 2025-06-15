6f1f7d5502fc50ea0d2f1bc18b98e5ef6a05538e
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2025-06-15 03:19:19 +0300

    Enhance TaskItem component with submission viewing functionality
    - Added a button to view submissions for each task, accessible to Teachers and Assistants.
    - Implemented a modal to display submission details, including student names, file links, and upload timestamps.
    - Introduced loading state management with a Spinner component during data fetching.
    - Improved code consistency by updating class attributes to use 'className' in JSX.

--- src/Components/TaskItem.js
+++ src/Components/TaskItem.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import EditTaskModal from "./EditTaskModal";
 import SubmitTaskModal from "./SubmitTaskModal";
-import { patchDataToken } from "../axios/axiosHelper";
+import { getData, patchDataToken } from "../axios/axiosHelper";
+import { Link } from "react-router-dom";
+import { Task_API_URL } from "../utils/constants";
+import toastMsg from "../functions/toastMsg";
+import Spinner from "../shared/Spinner";
 
 const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isSubmitModalOpen, setIsSubmitModalOpen] = useState(false);
+  const [isSubmissionsModalOpen, setIsSubmissionsModalOpen] = useState(false);
+  const [submissions, setSubmissions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleEdit = () => {
     setIsEditModalOpen(true);
@@ -27,10 +34,20 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
     onSubmit(file, task._id);
   };
 
+  const handleViewSubmissions = async () => {
+    setIsLoading(true);
+    setIsSubmissionsModalOpen(true);
+    await getData(Task_API_URL + "/" + task._id + "/submissions", true)
+      .then((res) => setSubmissions(res.data.data))
+      .catch((err) => toastMsg(err.response.data.message, "error"))
+      .finally(() => setIsLoading(false));
+  };
+
   // const isExternalLink = task.isLink && task.file;
 
   return (
     <article className="section-block">
+      {isLoading && <Spinner />}
       <div className="lecture-block h-100 w-100 d-flex flex-column justify-content-between">
         {/* <h2> */}
         <div className="row justify-content-center text-center">
@@ -40,7 +57,7 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
             rel="noopener noreferrer"
             className="pdf-link"
           >
-            <i class="bi bi-file-earmark-pdf text-danger"></i> {task.name}
+            <i className="bi bi-file-earmark-pdf text-danger"></i> {task.name}
           </a>
         </div>
         {task.solutionFile && (
@@ -51,7 +68,7 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
               rel="noopener noreferrer"
               className="pdf-link"
             >
-              <i class="bi bi-file-earmark-pdf text-success"></i> Solution
+              <i className="bi bi-file-earmark-pdf text-success"></i> Solution
             </a>
           </div>
         )}
@@ -60,6 +77,15 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
         </p>
         {(userRole === "Teacher" || userRole === "Assistant") && (
           <div className="row justify-content-center ">
+            <div className="col-6 text-center">
+              <button
+                className="btn btn-secondary w-50"
+                onClick={handleViewSubmissions}
+                title="Submissions"
+              >
+                <i className="bi bi-send-fill"></i>
+              </button>
+            </div>
             {userRole === "Assistant" && (
               <div className="col-6 text-center">
                 <button
@@ -67,7 +93,7 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
                   onClick={handleEdit}
                   title="Edit Task"
                 >
-                  <i class="bi bi-pencil-fill"></i>
+                  <i className="bi bi-pencil-fill"></i>
                 </button>
               </div>
             )}
@@ -77,7 +103,7 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
                 onClick={() => onDelete(index, task._id)}
                 title="Delete Task"
               >
-                <i class="bi bi-trash-fill"></i>
+                <i className="bi bi-trash-fill"></i>
               </button>
             </div>
           </div>
@@ -109,6 +135,74 @@ const TaskItem = ({ task, index, userRole, onDelete, onUpdate, onSubmit }) => {
         onSubmit={handleFileSubmit}
         taskId={task._id}
       />
+      {isSubmissionsModalOpen && (
+        <div className="modal fade show d-block" tabIndex="-1" role="dialog">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header bg-light">
+                <h5 className="modal-title fw-bold">Assignment Submissions</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setIsSubmissionsModalOpen(false)}
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body bg-white">
+                {submissions ? (
+                  <div className="">
+                    <div className="table-responsive">
+                      <table className="table table-hover">
+                        <thead className="table-light">
+                          <tr>
+                            <th scope="col">Student</th>
+                            <th scope="col">File</th>
+                            <th scope="col">Uploaded At</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {submissions.map((result, index) => (
+                            <tr key={result._id}>
+                              <td>{result.student.fullName}</td>
+                              <td>
+                                <span className="badge">
+                                  <Link to={result.file}>File</Link>
+                                </span>
+                              </td>
+                              <td>
+                                <span className="badge bg-warning">
+                                  {new Date(
+                                    result.createdAt
+                                  ).toLocaleDateString() +
+                                    " " +
+                                    new Date(
+                                      result.createdAt
+                                    ).toLocaleTimeString()}
+                                </span>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <div className="modal-footer bg-light">
+                <button
+                  type="button"
+                  className="btn btn-secondary px-4"
+                  onClick={() => setIsSubmissionsModalOpen(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </article>
   );
 };
